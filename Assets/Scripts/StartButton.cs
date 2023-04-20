d7637a5e0e91dcaa245cd737f220ecbf94ef6337
collinsmachar <cmacharia635@gmail.com>
2023-04-20 14:42:47 +0200

    start button code

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -8,16 +8,19 @@ using Unity.VisualScripting;
 public class StartButton : MonoBehaviour
 {
     [SerializeField] private Button startGame;
-    private int numberofPlayers;
-
-   public void StartGame()
-    {
-        
-        numberofPlayers++;
-    }
     
+    private int currentNumberOfPlayers;
+    private int currentNumberOfAccepted;
+    public void Start()
+    {
+        startGame.onClick.AddListener(OnStartButtonClickedServerRpc);
+    }
+    [ServerRpc]
+    private void OnStartButtonClickedServerRpc()
+    {
+        currentNumberOfAccepted++;
+        Debug.Log("start");
+    }
 
-
-   
 }
 
