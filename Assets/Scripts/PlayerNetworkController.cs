1403f287606d238a5161963cab08662206fbeeea
ayxan386 <ayxanpro@gmail.com>
2023-04-24 17:54:32 +0200

    Resetting asnwers

--- Assets/Scripts/PlayerNetworkController.cs
+++ Assets/Scripts/PlayerNetworkController.cs
@@ -49,7 +49,6 @@ public class PlayerNetworkController : NetworkBehaviour
         if (playerId == OwnerClientId)
             ShowMessage(IsOwner ? "You won!!" : $"{playerName.Value} won\n the word was {ans}");
 
-        controller.ResetAnswers();
         if (IsOwner) NetworkButtonManager.Instance.SetPlayerWaitingLayout();
     }
 
@@ -103,6 +102,7 @@ public class PlayerNetworkController : NetworkBehaviour
         print("Drawer selection event received: " + drawerId);
         isDrawer = OwnerClientId == drawerId;
         controller.UpdateBackgroundColor(isDrawer);
+        controller.ResetAnswers();
         if (isDrawer)
         {
             ShowMessage(IsOwner ? "You are the drawer" : $"{playerName.Value} is the drawer!!!");
@@ -118,6 +118,7 @@ public class PlayerNetworkController : NetworkBehaviour
             answerInputField.onSubmit.AddListener((ans) =>
             {
                 answerInputField.text = "";
+                answerInputField.Select();
                 OnPlayerAnswerSubmitServerRpc(ans);
             });
         }
